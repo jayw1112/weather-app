d0f052be590d01144362058e2c7dbb42bb563987
jayw1112 <housexpresso@gmail.com>
2022-08-16 18:18:56 -0700

    finished v1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Card from './components/Card'
 function App() {
   return (
     <div className='App'>
-      <h1>Weather</h1>
       <Card />
     </div>
   )
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,30 +1,64 @@
-// import { useState } from 'react'
-// import axios from 'axios'
+import { useState } from 'react'
+import axios from 'axios'
+import classes from './Weather.module.css'
 
-//   const url = `https://api.openweathermap.org/data/2.5/weather?q=hollywood&appid=6dad2483b2f50264a7d93d178dbc1110`
+const Weather = () => {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
 
-const Weather = () => (
-  <div className='container'>
-    <div className='top'>
-      <div className='name'>
-        <p>Hollywood</p>
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=6dad2483b2f50264a7d93d178dbc1110`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+  return (
+    <div className={classes.container}>
+      <div className={classes.search}>
+        <input
+          className={classes.input}
+          type='text'
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+        />
       </div>
-      <div className='temp'>
-        <h1>89F</h1>
+      <div className={classes.top}>
+        <div className={classes.name}>
+          <h2>{data.name}</h2>
+        </div>
+        <div className={classes.temp}>
+          {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+        </div>
+        <div className={classes.description}>
+          {data.weather ? <p>{data.weather[0].main}</p> : null}
+        </div>
       </div>
-      <div className='description'>
-        <p>Clear Sky</p>
-      </div>
+
+      {data.name != undefined && (
+        <div className={classes.bottom}>
+          <div className={classes.feels}>
+            {data.main ? <p>{data.main.feels_like.toFixed()}°F</p> : null}
+            <p className={classes.detail}>Feels Like</p>
+          </div>
+          <div className={classes.humidity}>
+            {data.main ? <p>{data.main.humidity}%</p> : null}
+            <p className={classes.detail}>Humidity</p>
+          </div>
+          <div className={classes.wind}>
+            {data.wind ? <p>{data.wind.speed.toFixed()}MPH</p> : null}
+            <p className={classes.detail}>Wind Speed</p>
+          </div>
+        </div>
+      )}
     </div>
-    <div className='bottom'>
-      <div className='feels'>
-        <p>85F</p>
-      </div>
-      <div className='humidity'>
-        <p>44</p>
-      </div>
-    </div>
-  </div>
-)
+  )
+}
 
 export default Weather
